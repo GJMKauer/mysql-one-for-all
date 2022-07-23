3170f84c6fc6096e993e26da79c59d8e1a5864d8
GabrielKauerWSL <gjmkauer@hotmail.com>
2022-07-23 01:02:18 -0300

    Adiciona requisito 3 - faz uma query que busca o nome do usuario, o total de musicas que ele ouviu e o tempo de musicas ouvidas.

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+SELECT 
+    u.user_name AS usuario,
+    COUNT(DISTINCT s.song_name) AS qtde_musicas_ouvidas,
+    CAST(SUM(s.song_duration / 60) AS DECIMAL (4 , 2 )) AS total_minutos
+FROM
+    SpotifyClone.users AS u
+        INNER JOIN
+    SpotifyClone.reproduction_history AS rh ON u.user_id = rh.user_id
+        INNER JOIN
+    SpotifyClone.songs AS s ON rh.song_id = s.song_id
+GROUP BY u.user_id
+ORDER BY 1 ASC;
