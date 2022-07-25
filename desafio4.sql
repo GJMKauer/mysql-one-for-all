8cd02d28c9e31b220785f82df92a8b2d648691a6
GabrielKauerWSL <gjmkauer@hotmail.com>
2022-07-25 16:08:49 -0300

    Refatora formatação do requisito 4, adiciona requisito 5 - busca as músicas mais ouvidas.

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,12 @@
-SELECT DISTINCT u.user_name as usuario,
-CASE
-	WHEN MAX(YEAR(reproduction_date)) = 2021 THEN 'Usuário ativo'
-    ELSE 'Usuário inativo'
-END AS condicao_usuario FROM SpotifyClone.users as u
-INNER JOIN SpotifyClone.reproduction_history as rh
-ON u.user_id = rh.user_id
+SELECT DISTINCT
+    u.user_name AS usuario,
+    CASE
+        WHEN MAX(YEAR(reproduction_date)) = 2021 THEN 'Usuário ativo'
+        ELSE 'Usuário inativo'
+    END AS condicao_usuario
+FROM
+    SpotifyClone.users AS u
+        INNER JOIN
+    SpotifyClone.reproduction_history AS rh ON u.user_id = rh.user_id
 GROUP BY u.user_name
 ORDER BY usuario;
---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+    s.song_name AS cancao, COUNT(rh.song_id) AS reproducoes
+FROM
+    SpotifyClone.songs AS s
+        INNER JOIN
+    SpotifyClone.reproduction_history AS rh ON s.song_id = rh.song_id
+GROUP BY s.song_name
+ORDER BY reproducoes DESC , cancao ASC
+LIMIT 2;
