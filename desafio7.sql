404d79f5169d99c00094f802de01e8915cd66c06
GabrielKauerWSL <gjmkauer@hotmail.com>
2022-07-25 16:51:50 -0300

    Adiciona requisito 7 - query que retorna os artistas com seus respectivos álbuns e quantidade de seguidores.

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+    ar.artist_name AS artista,
+    al.album_name AS album,
+    COUNT(DISTINCT fl.user_id) AS seguidores
+FROM
+    SpotifyClone.artists AS ar
+        INNER JOIN
+    SpotifyClone.albums AS al ON ar.artist_id = al.artist_id
+        INNER JOIN
+    SpotifyClone.follow_list AS fl ON ar.artist_id = fl.artist_id
+GROUP BY ar.artist_id , al.album_id
+ORDER BY seguidores DESC , artista ASC , album ASC;
